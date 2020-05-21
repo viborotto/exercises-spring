43d272ce72e9c2d485a8cd309abfb6a323463bbf
viborotto <vittoriaborotto@gmail.com>
2020-05-21 14:34:42 -0300

    Servico de ler Localizacao

--- funcionarios/src/main/java/com/devi/funcionarios/service/LocalizacaoService.java
+++ funcionarios/src/main/java/com/devi/funcionarios/service/LocalizacaoService.java
@@ -1,4 +1,16 @@
 package com.devi.funcionarios.service;
 
+import com.devi.funcionarios.models.Localizacao;
+import com.devi.funcionarios.repositories.LocalizacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LocalizacaoService {
+
+    @Autowired
+    private LocalizacaoRepository localizacaoRepository;
+
+    //ler localizacao
+    public Iterable<Localizacao> buscarTodos(){
+        return localizacaoRepository.findAll();
+    }
 }
