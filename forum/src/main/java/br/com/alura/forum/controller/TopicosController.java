4ce6a47c6abea63f97c1f4cb3ee572a8d6ddaba9
viborotto <vittoriaborotto@gmail.com>
2020-06-01 14:45:07 -0300

    Fim Forum

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,25 +2,43 @@ package br.com.alura.forum.controller;
 
 
 import br.com.alura.forum.dto.TopicoDto;
+import br.com.alura.forum.dto.TopicoForm;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
-    public List<Topico> lista(){
-        List<Topico> topicos = topicoRepository.findAll();
-        return topicos;
+    //filtrar
+    //localhost:8080/topicos?nomeCurso=Spring+Boot
+    @GetMapping
+    public List<Topico> lista(String nomeCurso){
+        if (nomeCurso==null){
+            List<Topico> topicos = topicoRepository.findAll();
+            return topicos;
+        } else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return topicos;
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Topico> cadastrar(@RequestBody Topico topico, UriComponentsBuilder uriBuilder){
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(topicoRepository.save(topico));
     }
 }
--- forum/src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repositories/TopicoRepository.java
@@ -3,5 +3,9 @@ package br.com.alura.forum.repositories;
 import br.com.alura.forum.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    //atributo de Curso que é relacionamento de Topico
+    List<Topico> findByCursoNome(String nomeCurso);
 }
