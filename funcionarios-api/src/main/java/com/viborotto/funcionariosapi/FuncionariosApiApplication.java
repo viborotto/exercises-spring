deea1bd4d38e6596a81f703604cbfc1f9e63ca00
viborotto <vittoriaborotto@gmail.com>
2020-07-31 21:13:52 -0300

    Adicionado projeto funcionarios-api

---
+++ funcionarios-api/src/main/java/com/viborotto/funcionariosapi/FuncionariosApiApplication.java
@@ -0,0 +1,13 @@
+package com.viborotto.funcionariosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncionariosApiApplication.class, args);
+    }
+
+}
---
+++ funcionarios-api/src/test/java/com/viborotto/funcionariosapi/FuncionariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viborotto.funcionariosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionariosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
